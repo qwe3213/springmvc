697a3ebd70f40095d0763e789d2bce6cd9d0dabb
hayun <hs183000@naver.com>
2023-10-30 23:35:54 +0900

    로깅 간단한 설정2

--- src/main/java/hello/springmvc/basic/LogTestController.java
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -1,10 +1,11 @@
 package hello.springmvc.basic;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+//@Slf4j
 @RestController
 public class LogTestController {
     private final Logger log = LoggerFactory.getLogger(getClass());
@@ -16,7 +17,8 @@ public class LogTestController {
 
         System.out.println("name = " + name);
 
-        log.info("info log={}", name);
+        log.info("info log={}"+ name);
+        // 더하기 연산이 일어나 사용하지 않을때에도 + 연산이 일어나 리소스 낭비가 일어남 그러므로 , 를 사용하는 것 이 좋음.
         log.debug("debug log={}",name);
         log.info("info log={}",name);
         log.warn("info log={}", name);
